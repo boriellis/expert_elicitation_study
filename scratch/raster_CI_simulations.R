946df7476d32671d569ec368ad5d6e3f0a24ea31
boriellis <aaellis@ucsc.edu>
2025-12-05 14:43:20 -0800

    notes on spatial analysis

--- scratch/raster_CI_simulations.R
+++ scratch/raster_CI_simulations.R
@@ -55,16 +55,26 @@ x <- c(
   rtruncnorm(round(n * w2), a = 0, b = 1, mean = 0.8,  sd = 0.10)
 )
 
-hist(x, breaks = 10, main = "Bimodal Distribution (Truncated Normals)")
+logit_x <- log(x / (1 - x))
+mean_logit_x <- mean(logit_x)
+sd_logit_x <- sd(logit_x)
+lwr_logit_x <- mean_logit_x - 1.96 * sd_logit_x
+upr_logit_x <- mean_logit_x + 1.96 * sd_logit_x
+inv_logit <- function(x) exp(x) / (1 + exp(x))
+mean_x <- inv_logit(mean_logit_x)
+lwr_x <- inv_logit(lwr_logit_x)
+upr_x <- inv_logit(upr_logit_x)
 
-m <- mean(x)
-ci <- t.test(x)$conf.int  # 95% CI
+# hist(x, breaks = 10, main = "Bimodal Distribution (Truncated Normals)")
+# 
+# m <- mean(x)
+# ci <- t.test(x)$conf.int  # 95% CI
 
 # plot histogram with mean & CI
 hist(x, breaks = 10)
-abline(v = m, col = "red", lwd = 2)
-abline(v = ci, col = "blue", lty = 2, lwd = 2)
+abline(v = mean_x, col = "red", lwd = 2)
+abline(v = c(lwr_x, upr_x), col = "blue", lty = 2, lwd = 2)
 
-m; ci
+c(mean_x, lwr_x, upr_x)
 
 
